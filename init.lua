3d1fd49aab74546690deccd47a59ca0c3f1816c2
Adelgrin <lamaral2002@gmail.com>
2025-09-11 08:06:17 -0300

    sniprun e nvim tree

--- init.lua
+++ init.lua
@@ -187,7 +187,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+--vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('n', '<C-n>', function()
+  require('nvim-tree.api').tree.toggle()
+end, { desc = 'Toggle nvim-tree', silent = true })
 
 -- TIP: Disable arrow keys in normal mode
 -- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -5,4 +5,6 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
+  check_ts = true,
+  map_cr = true,
 }
